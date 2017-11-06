63fe95b436780f47b44ae3ec35d0ef9af8c9cd86
Matheus Moura <matheus.moura@prodeaf.net>
2017-11-06 15:26:02 -0300

    refactor: FSM implementada na torre

--- Assets/Scripts/Tower.cs
+++ Assets/Scripts/Tower.cs
@@ -4,17 +4,51 @@ using UnityEngine;
 
 public class Tower : MonoBehaviour {
 
+	public enum Stats
+	{
+		SEARCHING,
+		SHOOTING,
+		KILLED
+	}
+
 	public Transform cannonTransform;
 	public Transform targetEnemy;
 	public Shooter shooter;
 	public float activationDistance=5;
+	private Stats currentStat;
 
 	void Update () {
-		if (Vector3.Distance (transform.position, targetEnemy.position) <= activationDistance) {
-			Vector3 position = targetEnemy.position;
-			position.y = cannonTransform.position.y;
-			cannonTransform.LookAt (position);
-			shooter.ShootAt (targetEnemy);
+		switch (currentStat) {
+			case Stats.SEARCHING:
+				Search ();
+				break;
+			case Stats.SHOOTING:
+				Shoot ();
+				break;
+			case Stats.KILLED:
+				Kill ();
+				break;
 		}
+	}
+
+	private void Search(){
+		cannonTransform.Rotate (0, 25 * Time.deltaTime, 0);
+
+		if (Vector3.Distance (transform.position, targetEnemy.position) <= activationDistance)
+			currentStat = Stats.SHOOTING;
+	}
+
+	private void Shoot(){
+		Vector3 position = targetEnemy.position;
+		position.y = cannonTransform.position.y;
+		cannonTransform.LookAt (position);
+		shooter.ShootAt (targetEnemy);
+		
+		if (Vector3.Distance (transform.position, targetEnemy.position) > activationDistance)
+			currentStat = Stats.SEARCHING;
+	}
+
+	private void Kill(){
+
 	}
 }
