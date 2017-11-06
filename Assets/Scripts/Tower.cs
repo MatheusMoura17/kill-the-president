db8ea3fb753b4d588b94718d15a8cd8ddfa2cc76
Matheus Moura <matheus.moura@prodeaf.net>
2017-11-06 12:58:51 -0300

    feat: Canhão da torre olhando para o inimigo

---
+++ Assets/Scripts/Tower.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tower : MonoBehaviour {
+
+	public Transform cannonTransform;
+	public Transform targetEnemy;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 position = targetEnemy.position;
+		position.y = cannonTransform.position.y;
+		cannonTransform.LookAt (position);
+	}
+}
