fd263fa319cde2d8bb930884d32ee063a53d5a5e
Matheus Moura <matheus.moura@prodeaf.net>
2017-11-06 15:12:10 -0300

    feat: BulletSpawner criado

---
+++ Assets/Scripts/BulletSpawner.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletSpawner : MonoBehaviour {
+
+	public static BulletSpawner instance;
+
+	public GameObject bulletPrefab;
+	private List<GameObject> spawnedBullets;
+
+	void Awake () {
+		spawnedBullets = new List<GameObject> ();
+		instance = this;
+	}
+
+	public GameObject SpawnBullet(Vector3 position, Quaternion rotation){
+		foreach (GameObject go in spawnedBullets)
+			if (!go.activeSelf) {
+				go.transform.position = position;
+				go.transform.rotation = rotation;
+				go.SetActive (true);
+				return go;
+			}
+
+		GameObject tmpGo=Instantiate (bulletPrefab,position,rotation,transform);
+		spawnedBullets.Add(tmpGo);
+		return tmpGo;
+	}
+
+}
