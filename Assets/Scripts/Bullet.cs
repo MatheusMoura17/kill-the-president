9e1926592daf69598279e98b5439b2ff0a246de0
Matheus Moura <matheus.moura@prodeaf.net>
2017-11-06 15:11:54 -0300

    feat: Script da bala implementado

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+
+	public float speed=10;
+	public Transform targetTransform;
+
+	void Update () {
+		if (targetTransform) {
+			transform.position = Vector3.MoveTowards (
+				transform.position, 
+				targetTransform.position, 
+				speed * Time.deltaTime);
+
+			if ((int)Vector3.Distance (targetTransform.position, transform.position) == 0) {
+				targetTransform = null;
+				gameObject.SetActive (false);
+			}
+		}
+	}
+}
