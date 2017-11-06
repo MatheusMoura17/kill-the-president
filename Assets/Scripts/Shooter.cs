647ea3a0fa7ed89916fff2c8293383df392c64ee
Matheus Moura <matheus.moura@prodeaf.net>
2017-11-06 13:26:53 -0300

    feat: Script Shooter criado e adicionado a torre

---
+++ Assets/Scripts/Shooter.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooter : MonoBehaviour {
+
+	public GameObject shootPrefab;
+	public Transform spawnPoint;
+	public float fireRate = 2;
+	private float fireTimer=0;
+	private bool canShoot;
+
+	void Update () {
+		if (!canShoot) {
+			fireTimer += Time.deltaTime;
+			canShoot = fireTimer >= fireRate;
+		}
+	}
+
+	void ResetShoot(){
+		fireTimer = 0;
+		canShoot = false;
+	}
+
+	public void Shoot(){
+		if (canShoot) {
+			Instantiate (shootPrefab, spawnPoint.position, spawnPoint.rotation);
+			ResetShoot ();
+		}
+	}
+}
