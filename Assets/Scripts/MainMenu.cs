28131689203894acc530d992c1912a7aff643a6a
Alexsandro Rodrigues <thealeehmy@gmail.com>
2017-11-07 16:02:52 -0300

    Feat: Menu do jogo

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+    public Button jogar;
+
+	// Use this for initialization
+	void Start () {
+
+        jogar = jogar.GetComponent<Button>();
+	}
+	
+	public void GameplayLevel()
+    {
+        SceneManager.LoadScene("Gameplay (Teste)");
+    }
+}
