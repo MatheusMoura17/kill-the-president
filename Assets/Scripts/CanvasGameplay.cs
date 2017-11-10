b45432deefa9acde0c070c54dcb983ee892a1dcc
Alexsandro Rodrigues <thealeehmy@gmail.com>
2017-11-10 01:28:27 -0300

    Feat: Adcionando ao script de event, as funcionalidades da gameplay

--- Assets/Scripts/CanvasGameplay.cs
+++ Assets/Scripts/CanvasGameplay.cs
@@ -7,6 +7,9 @@ public class CanvasGameplay : MonoBehaviour {
 
     public Canvas telaGameOver;
     public Canvas telaVitoria;
+    public Canvas gameplay;
+
+    public Button militantes;
 
     public Button over;
     public Button Winner;
@@ -16,22 +19,28 @@ public class CanvasGameplay : MonoBehaviour {
 
         telaGameOver = GetComponent<Canvas>();
         telaVitoria = GetComponent<Canvas>();
+        gameplay = GetComponent<Canvas>();
+
+        militantes = GetComponent<Button>();
 
         over = GetComponent<Button>();
         Winner = GetComponent<Button>();
 
+        gameplay.enabled = true;
         telaVitoria.enabled = false;
         telaGameOver.enabled = false;
 	}
 	
 	public void GameOverPress()
     {
+        gameplay.enabled = false;
         telaGameOver.enabled = true;
         telaVitoria.enabled = false;
     }
 
     public void TelaVitoriaPress()
     {
+        gameplay.enabled = false;
         telaVitoria.enabled = true;
         telaGameOver.enabled = false;
     }
