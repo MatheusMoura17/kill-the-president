7154c94835521256ea4cd9dd543d9e516bd11521
Matheus Moura <matheus.moura@prodeaf.net>
2017-11-10 05:32:40 -0300

    feat: Movimentação do meliante implementada

--- Assets/Scripts/SimpleMove.cs
+++ Assets/Scripts/SimpleMove.cs
@@ -1,9 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class SimpleMove : MonoBehaviour {
 
+	public TargetLooker targetLooker;
+	public NavMeshAgent agent;
 	// Use this for initialization
 	void Start () {
 		
@@ -11,6 +14,8 @@ public class SimpleMove : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		transform.Translate (0, 0, 1 * Time.deltaTime);
+		if (targetLooker.target)
+			agent.destination = targetLooker.target.transform.position;
+		//transform.Translate (0, 0, 1 * Time.deltaTime);
 	}
 }
