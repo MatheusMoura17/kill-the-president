1453e69736c96e50ccd7273f3eeb5f90959f82e4
Alexsandro Rodrigues <thealeehmy@gmail.com>
2017-11-10 01:00:26 -0300

    Feat: Configurando o scripts de event para as telas de vitoria e game over

---
+++ Assets/Scripts/CanvasGameplay.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using UnityEngine.UI;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CanvasGameplay : MonoBehaviour {
+
+    public Canvas telaGameOver;
+    public Canvas telaVitoria;
+
+    public Button over;
+    public Button Winner;
+
+	// Use this for initialization
+	void Start () {
+
+        telaGameOver = GetComponent<Canvas>();
+        telaVitoria = GetComponent<Canvas>();
+
+        over = GetComponent<Button>();
+        Winner = GetComponent<Button>();
+
+        telaVitoria.enabled = false;
+        telaGameOver.enabled = false;
+	}
+	
+	public void GameOverPress()
+    {
+        telaGameOver.enabled = true;
+        telaVitoria.enabled = false;
+    }
+
+    public void TelaVitoriaPress()
+    {
+        telaVitoria.enabled = true;
+        telaGameOver.enabled = false;
+    }
+
+    public void MainMenuLevel()
+    {
+        SceneManager.LoadScene("Main Menu");
+    }
+}
