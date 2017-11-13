95c4f2c3bd51f4a4fe10adc72208c82d25edbba3
Matheus Moura <matheus.moura@prodeaf.net>
2017-11-13 02:21:46 -0300

    feat: Arvore sendo treinada

---
+++ Assets/Scripts/TreeDecision.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Data;
+using System.IO;
+using Accord.Statistics.Filters;
+using Accord.Math;
+using Accord.MachineLearning.DecisionTrees.Learning;
+using Accord.MachineLearning.DecisionTrees;
+using Accord.Math.Optimization.Losses;
+using Accord.MachineLearning;
+using System.Threading;
+using ExcelDataReader;
+using System;
+
+public class TreeDecision : MonoBehaviour {
+
+	public string trainData;
+	public string treeLocation;
+	DecisionTree tree;
+	Codification codebook;
+
+	// Use this for initialization
+	void Start () {
+		Train ();
+	}
+
+	private void Train() {
+		DataTable data = GetDataTable(Application.dataPath +"/"+ trainData);
+		DebugTable(data);
+		codebook = new Codification(data);
+		DataTable symbols = codebook.Apply(data);
+
+		int[][] inputs = symbols.ToArray<int>("LIFE","TOWERS","MELIANTS","TIME","ENEMY_COINS");
+		int[] outputs = symbols.ToArray<int>("POSITION");
+
+		var id3learning = new ID3Learning();
+		id3learning.Attributes = DecisionVariable.FromData(inputs);
+
+		tree = id3learning.Learn(inputs, outputs);
+
+		double error = new ZeroOneLoss(outputs).Loss(tree.Decide(inputs));
+		tree.Save(Application.dataPath +"/"+ treeLocation);
+	}
+
+	public void DebugTable(DataTable table) {
+		string a = "";
+		a += ("--- DebugTable(" + table.TableName + ") ---\n");
+		int zeilen = table.Rows.Count;
+		int spalten = table.Columns.Count;
+
+		// Header
+		for (int i = 0; i < table.Columns.Count; i++) {
+			string s = table.Columns[i].ToString();
+			a += (String.Format("{0,-20} | ", s));
+		}
+		a += (Environment.NewLine);
+		for (int i = 0; i < table.Columns.Count; i++) {
+			a += ("---------------------|-");
+		}
+		a += (Environment.NewLine);
+
+		// Data
+		for (int i = 0; i < zeilen; i++) {
+			DataRow row = table.Rows[i];
+			//Debug.WriteLine("{0} {1} ", row[0], row[1]);
+			for (int j = 0; j < spalten; j++) {
+				string s = row[j].ToString();
+				if (s.Length > 20) s = s.Substring(0, 17) + "...";
+				a += (String.Format("{0,-20} | ", s));
+			}
+			a += (Environment.NewLine);
+		}
+		for (int i = 0; i < table.Columns.Count; i++) {
+			a += ("---------------------|-");
+		}
+		a += (Environment.NewLine);
+		Debug.Log(a);
+	}
+
+	private DataTable GetDataTable(string patch) {
+		DataTable dt = new DataTable();
+
+		FileStream stream = File.Open(patch, FileMode.Open, FileAccess.Read);
+		IExcelDataReader excelReader;
+
+		if (patch.Contains(".xlsx"))
+			excelReader = ExcelReaderFactory.CreateOpenXmlReader(stream);
+		else
+			excelReader = ExcelReaderFactory.CreateBinaryReader(stream);
+
+		DataSet result = excelReader.AsDataSet(new ExcelDataSetConfiguration() {
+			ConfigureDataTable = (tableReader) => new ExcelDataTableConfiguration() {
+				UseHeaderRow = true,
+			}
+		});
+		excelReader.Close();
+
+		return result.Tables[0];
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
